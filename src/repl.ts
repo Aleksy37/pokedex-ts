da45162094c9f6470dd3350ff9da9dcfe5012d46
Aleksy37 <aleksyveillette@gmail.com>
2026-03-01 23:33:58 -0500

    startrepl func

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,23 @@
+import { createInterface } from 'node:readline';
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex >"
+    });
+    rl.prompt();
+    rl.on("line", (input) => {
+        const res = cleanInput(input)
+        if (res.length === 0) {
+            rl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${res[0]}`);
+        rl.prompt();
+    })
+}
+
 export function cleanInput(input: string): string[] {
     const clean = input.trim().toLowerCase()
     if (clean === "") {
